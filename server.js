35d61622c9633446a3341aeba1e34b786b3732eb
Reginald Robinson <ddrbnsn@gmail.com>
2023-02-13 18:18:44 -0800

    Doing init stuff wo a mouse

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express'),
+app = express(),
+port = process.env.PORT || 3012,
+server = app.listen(port, () => console.log(`Go Away!! (...perhaps to port${port})`))
+app.use(express.static('public'))
