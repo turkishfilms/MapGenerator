cff14965b30f1d558617e27f652e47359b92b497
Reginald Robinson <ddrbnsn@gmail.com>
2023-03-05 16:55:53 -0800

    ttok out old stuff

--- public/index.js
+++ public/index.js
@@ -24,24 +24,13 @@ let width,
 
 function setup() {
     noStroke()
-    // width = windowWidth
-    // height = windowHeight
-    width = 100
-    height = 100
+    width = windowWidth
+    height = windowHeight
+    // width = 100
+    // height = 100
     createCanvas(width, height)
     background(0)
     pd = pixelDensity(1)._pixelDensity
-    // borderMapSize = height * pd * width * pd * 4
-
-    // const starting = 100
-
-    // const CELLLIST = [{ num: 0, mute: starting },
-    //     { num: 1, mute: starting }, { num: 2, mute: starting }, { num: 3, mute: starting }
-    // ]
-
-    // const FIRST = CELLLIST[0]
-
-    // distList = rn(0.1, bn(5), "both")
     distList = island2()
     console.log("Showed")
 }
@@ -49,14 +38,19 @@ function setup() {
 
 /**Creates an island2
  *@function 
-@desc males an isalnd in the pixels array
-@param { Number } inc -Increment value for perlin noise, affects "smoothness"
-@param { Number } fallOff -The "steepness" of the border map boundary
-@param { Array[2] } center -Coords of island's center 
-@param { Number } seed -seed for noise()
-@param { Number } squareness -How sqaure the border is
+ * @desc males an isalnd in the pixels array
+ * @param { Number } inc -Increment value for perlin noise, affects "smoothness"
+ * @param { Number } fallOff -The "steepness" of the border map boundary
+ * @param { Array[2] } center -Coords of island's center 
+ * @param { Number } seed -seed for noise()
+ * @param { Number } squareness -How sqaure the border is
+ * 
+ * @desc creates an array of length pixels.length/4 of values between 1 and 0. 1 in the center and tapering off radially to 0 by factor fallOff
+ * @issue if user can pick focus how do I algotihmically find the max Dist ( always in a corner: so do a quadrant check and pick "opposite quadrants corner") 
  */
-const island2 = (inc = 0.1, fallOff = 3, center = { x: (width / 2), y: (height / 2) },seed= random(100), squareness = 0) => {
+
+const island2 = (inc = 0.1, fallOff = 3, center = { x: (width / 2), y: (height / 2) }, seed = random(100), squareness = 0) => {
+    console.log("updating pixels")
     const maxDist = dist(0, 0, center.x, center.y)
     let yoff = 0;
     noiseSeed(seed)
@@ -77,451 +71,151 @@ const island2 = (inc = 0.1, fallOff = 3, center = { x: (width / 2), y: (height /
         yoff += inc;
     }
     updatePixels();
+    console.log("updated pixels")
 }
 
-
-
-/**Creates an island
- *@function 
-@desc males an isalnd in the pixels array
-@param { Number } inc -Increment value for perlin noise, affects "smoothness"
-@param { Number } fallOff -The "steepness" of the border map boundary
-@param { Array[2] } center -Coords of island's center 
-@param { Number } seed -seed for noise()
-@param { Number } squareness -How sqaure the border is
- */
-const island = (inc = 0.1, fallOff = 3, center = { x: (width / 2), y: (height / 2) },seed= random(100), squareness = 0) => {
-    const borderMap = []
-    const maxDist = dist(0, 0, center.x, center.y)
-
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            const distToC = dist(x, y, center.x, center.y)
-            const maskVal = map(distToC, 0, maxDist, 1, 0) ** fallOff
-            borderMap.push(maskVal);
-        }
-    }
-
-    let yoff = 0;
-    noiseSeed(seed)
-    loadPixels();
-    for (let y = 0; y < height; y++) {
-        let xoff = 0;
-        for (let x = 0; x < width; x++) {
-            const index = (x + y * width) * 4
-            const r = noise(xoff, yoff) * borderMap[index / 4]
-            pixels[index + 0] = r * 255;
-            pixels[index + 1] = r * 255;
-            pixels[index + 2] = r * 255;
-            pixels[index + 3] = 255;
-            xoff += inc;
-        }
-        yoff += inc;
-    }
-    updatePixels();
-
-
-}
-
-
-
-// wokring
-// let bn = (fallOff) => {
-//     const borderMap = []
-//     for (let y = 0; y < height; y++) {
-//         for (let x = 0; x < width; x++) {
-//             const distToC = dist(y, x, 50, 50)
-//             const maskVal = map(distToC, 0, 70, 1, 0) ** fallOff
-//             borderMap.push(maskVal);
-//             // borderMap.push(maskVal);
-//             // borderMap.push(maskVal);
-//             // borderMap.push(maskVal);
-//         }
-//     }
-//     return borderMap
-// }
-
-// let rn = (inc, mask, w) => {
-//     let yoff = 0;
-//     noiseSeed(35)
-//     loadPixels();
-//     for (let y = 0; y < height; y++) {
-//         let xoff = 0;
-//         for (let x = 0; x < width; x++) {
-//             const index = (x + y * width) * 4
-
-//             let r = 0
-//             if (w == "noise") r = noise(xoff, yoff)
-//             else if (w == "mask") r = mask[index/4]
-//             else r = noise(xoff, yoff) * mask[index/4]
-
-//             pixels[index + 0] = r * 255;
-//             pixels[index + 1] = r * 255;
-//             pixels[index + 2] = r * 255;
-//             pixels[index + 3] = 255;
-//             xoff += inc;
-//         }
-//         yoff += inc;
-//     }
-//     updatePixels();
-// }
-
-const bN = (fallOff, foc, squareness) => {
-    const borderMap = []
-    const focus = foc || { //const for now, Parameterize this later
-        x: width * pd / 2,
-        y: height * pd / 2
-    }
-    const maxDist = dist(0, 0, width * pd, height * pd)
-    for (let y = 0; y < height * pd; y++) {
-        for (let x = 0; x < width * pd; x++) {
-            const distToFocus = dist(x, y, focus.x, focus.y)
-            const maskVal = map(fallOff * distToFocus, 0, maxDist, 1, 0)
-            borderMap.push(maskVal)
-        }
-    }
-
-    console.log("killer killin")
-    return borderMap
-}
-
-const rs2 = (inc, mask, seed = 35) => {
-    const got = []
-    let yoff = 0;
-    noiseSeed(seed)
-    loadPixels();
-    for (let y = 0; y < height; y++) {
-        let xoff = 0;
-        for (let x = 0; x < width; x++) {
-            let index = (x + y * width) * 4;
-            // let r = random(255);
-            let r = 255 * mask[index];
-            pixels[index + 0] = r;
-            pixels[index + 1] = r;
-            pixels[index + 2] = r;
-            pixels[index + 3] = 255;
-            got.push(r)
-            xoff += inc;
-        }
-        yoff += inc;
-    }
-    updatePixels();
-    // return got
-}
-
-const ranshiff = (inc) => {
-    loadPixels()
-    const noise_ = []
-    let yoff = 0
-    let xoff = 0
-    let y_ = 0
-    let x_ = 0
-    for (let i = 0; i < borderMapSize; i += 4) { //long
-        const woah = noise(xoff, yoff)
-        noise_.push(woah)
-        pixels[i + 0] = woah * 255
-        pixels[i + 1] = woah * 255
-        pixels[i + 2] = woah * 255
-        pixels[i + 3] = 255
-        const x = i % (width * pd)
-        const y = floor(i / (width * pd))
-
-        if (x > x_) xoff += inc
-        else xoff = 0
-        if (y > y_) yoff += inc
-        y_ = y
-        x_ = x
-    }
-    console.log("randoming currently")
-    updatePixels()
-    return noise_
-}
-
-/**
- * Creates an array mask
- * @function
- * @param { Number } squareness 
- * @param { Number } fallOff
- * @desc creates an array of length pixels.length/4 of values between 1 and 0. 1 in the center and tapering off radially to 0 by factor fallOff
- * @issue if user can pick focus how do I algotihmically find the max Dist ( always in a corner: so do a quadrant check and pick "opposite quadrants corner") 
- */
-const danshiff = (inc) => {
-    let yoff = 0;
-    loadPixels();
-    for (let y = 0; y < height; y++) {
-        let xoff = 0;
-        for (let x = 0; x < width; x++) {
-            let index = (x + y * width) * 4;
-            // let r = random(255);
-            let r = noise(xoff, yoff) * 255;
-            pixels[index + 0] = r;
-            pixels[index + 1] = r;
-            pixels[index + 2] = r;
-            pixels[index + 3] = 255;
-
-            xoff += inc;
-        }
-        yoff += inc;
-    }
-    updatePixels();
-    // let me = []
-    // for (let i = 0; i < pixels.length; i++) me.push(pixels[i])
-    // return me
-}
-
-
-
-
-const duder = (mask) => {
-    loadPixels();
-    for (let y = 0; y < height * pd; y++) {
-        for (let x = 0; x < width * pd; x++) {
-            let indexo = (x + y * width);
-            let index = indexo * 4;
-            let r = mask[indexo] * 255;
-            pixels[index + 0] = r;
-            pixels[index + 1] = r;
-            pixels[index + 2] = r;
-            pixels[index + 3] = 255;
-        }
-    }
-    updatePixels();
-}
-
-/**
-pixels long
-noise long
-border long
-
- */
-
-
-const combine = (noise, mask) => {
-    loadPixels();
-    for (let y = 0; y < height * pd; y++) {
-        for (let x = 0; x < width * pd; x++) {
-            const maskIndex = (x + y * width * pd);
-            const noiseIndex = maskIndex * 4;
-            const val = noise[noiseIndex] //* mask[maskIndex]
-            pixels[noiseIndex + 0] = val;
-            pixels[noiseIndex + 1] = val;
-            pixels[noiseIndex + 2] = val;
-            pixels[noiseIndex + 3] = 255;
-        }
-    }
-    updatePixels();
-}
-
-const borderNoise = (fallOff, squareness) => {
-    const borderMap = []
-    const focus = { //const for now, Parameterize this later
-        x: width * pd / 2,
-        y: height * pd / 2
-    }
-    const maxDist = dist(0, 0, width * pd * 2, height * pd * 2)
-
-    for (let i = 0; i < borderMapSize; i++) {
-        const x = i % (width * pd)
-        const y = floor(i / (width * pd))
-        const distToFocus = dist(x, y, focus.x, focus.y)
-        const maskVal = map(fallOff * distToFocus, 0, maxDist / 2, 1, 0)
-        borderMap.push(maskVal)
-    }
-    console.log("bordermaskers be bordermaskin")
-    return borderMap
-}
-
-
-
-const noiseMapStuff = (config) => {
-    /**IN GENERAL HERES THE PLAN
-     * TODO: GEnerate "Smooth" Noise (height map) perlin/pixels
-     *  TODO: Generate border "noise" (border/isldn mask)
-     *  TODO: color everything
-     */
-
-    const smoothNoise = (smoothness) => {
-
-    }
-
-
-    const coloring = (input) => {
-        heightValToColor(input)
-    }
-
-    const heightValToColor = (heightVal) => {
-        stroke(255 * heightVal)
-    }
-    const loseerville = () => {
-        const n = 1
-        for (let i = 0; i < height / n; i++) {
-            for (let j = 0; j < width / n; j++) {
-                coloring(noise((i + config.x) * (j + config.y)))
-                point(j + width / n, i + height / n)
-            }
-        }
-    }
-    const thething = danshiff(config.x)
-    return thething
-}
-
-function touchStarted() {
-    background(0)
-    noiseSeed(mouseX)
-    noiseMapStuff({ x: 0.02, y: mouseY })
-}
+function touchStarted() {}
 
 function keyPressed() {
-    if (key == "n") updateGrid(grid)
-    if (key == " ") showGrid(grid)
-    if (key == "p") {
-        background(0)
-        noiseMapStuff({ x: (1 / mouseX) * 0.01, y: 1 / mouseY })
-        console.log("nope")
-    }
-    if (key == "m")
-        for (let i = 0; i < 100; i++) {
-            updateGrid(grid)
-            showGrid(grid)
-        }
-
+    if(key == " ") island2()
 }
 
-/**Creates a 2d array
- * @function
- * @param { Number } w - width of grid area in pixels
- * @param { Number } h - height of grid area in pixels
- * @param { Number } cellSize - height and width of each cell
- * @param { any } val - the backgorund value for the grid
- * @returns { Array } a 2d array
- */
-const genGrid = (w, h, cellSize, val) => {
-    const grid = []
-    const columns = floor(w / cellSize)
-    const rows = floor(h / cellSize)
-    for (let i = 0; i < columns; i++) {
-        let column = []
-        for (let j = 0; j < rows; j++) {
-            column.push(val)
-        }
-        grid.push(column)
-    }
-    return grid
-}
-
-/**Randomizes a grid with items from arr 
- * @function
- * @param { Array } grid - 2d array 
- * @param { Array } vals - Choices for the randomization
- * @returns { Array } - 2d array 
- */
-const ranGrid = (grid, vals) => {
-    const ranThresh = 1 / vals.length
-    const mixedGrid = grid.slice()
-    if (ranThresh != 1) {
-        mixedGrid.forEach((column, x) => {
-            column.forEach((cell, y) => {
-                mixedGrid[x][y] = vals[floor(random() / ranThresh)]
-            })
-        })
-    } else {
-        mixedGrid.forEach((column, x) => {
-            column.forEach((cell, y) => {
-                mixedGrid[x][y] = random()
-            })
-        })
-    }
-    return mixedGrid
-}
-
-/** Selects color for cell
- * @function
- * @param { Number } number - The value in a cell
- * @returns { Array } - Array[3] [r,g,b]
- */
-const correctColor = (number) => {
-    /**
-     * 0 is empty
-     * 1 is land
-     * 2 is water
-     * 3 is beach
+const gridBasedIsalnd = () => {
+    /**Creates a 2d array
+     * @function
+     * @param { Number } w - width of grid area in pixels
+     * @param { Number } h - height of grid area in pixels
+     * @param { Number } cellSize - height and width of each cell
+     * @param { any } val - the backgorund value for the grid
+     * @returns { Array } a 2d array
      */
-    switch (number) {
-        case 0:
-            fill(50)
-            break;
-        case 1:
-            fill(150, 100, 30)
-            break;
-        case 3:
-            fill(250, 200, 30)
-            break;
-        case 2:
-            fill(0, 25, 180)
-            break;
+    const genGrid = (w, h, cellSize, val) => {
+        const grid = []
+        const columns = floor(w / cellSize)
+        const rows = floor(h / cellSize)
+        for (let i = 0; i < columns; i++) {
+            let column = []
+            for (let j = 0; j < rows; j++) {
+                column.push(val)
+            }
+            grid.push(column)
+        }
+        return grid
     }
-}
 
-const showGrid = (grid, real) => {
-    // background(0)
-    if (real) {
-        grid.forEach((column, x) => {
-            column.forEach((cell, y) => {
-                correctColor(cell.num)
-                rect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE)
+    /**Randomizes a grid with items from arr 
+     * @function
+     * @param { Array } grid - 2d array 
+     * @param { Array } vals - Choices for the randomization
+     * @returns { Array } - 2d array 
+     */
+    const ranGrid = (grid, vals) => {
+        const ranThresh = 1 / vals.length
+        const mixedGrid = grid.slice()
+        if (ranThresh != 1) {
+            mixedGrid.forEach((column, x) => {
+                column.forEach((cell, y) => {
+                    mixedGrid[x][y] = vals[floor(random() / ranThresh)]
+                })
             })
-        })
-    } else {
-        grid.forEach((column, x) => {
-            column.forEach((cell, y) => {
-                fill(cell * 255)
-
-                rect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE)
+        } else {
+            mixedGrid.forEach((column, x) => {
+                column.forEach((cell, y) => {
+                    mixedGrid[x][y] = random()
+                })
             })
-        })
+        }
+        return mixedGrid
     }
-}
 
-const getNeighbors = (grid, x, y) => {
-    const neighbors = []
-    for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-            if (x == 0 || x == grid.length - 1 || y == 0 || y == grid[0].length - 1) neighbors.push({ num: -1, mute: 0 })
-            else {
-                const val = grid[x + i][y + j]
-                if (verb) console.log("x,", x, "y", y, i, j, val)
-                neighbors.push(val)
+    /** Selects color for cell
+     * @function
+     * @param { Number } number - The value in a cell
+     * @returns { Array } - Array[3] [r,g,b]
+     */
+    const correctColor = (number) => {
+        /**
+         * 0 is empty
+         * 1 is land
+         * 2 is water
+         * 3 is beach
+         */
+        switch (number) {
+            case 0:
+                fill(50)
+                break;
+            case 1:
+                fill(150, 100, 30)
+                break;
+            case 3:
+                fill(250, 200, 30)
+                break;
+            case 2:
+                fill(0, 25, 180)
+                break;
+        }
+    }
+
+    const showGrid = (grid, real) => {
+        // background(0)
+        if (real) {
+            grid.forEach((column, x) => {
+                column.forEach((cell, y) => {
+                    correctColor(cell.num)
+                    rect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE)
+                })
+            })
+        } else {
+            grid.forEach((column, x) => {
+                column.forEach((cell, y) => {
+                    fill(cell * 255)
+
+                    rect(x * CELLSIZE, y * CELLSIZE, CELLSIZE, CELLSIZE)
+                })
+            })
+        }
+    }
+
+    const getNeighbors = (grid, x, y) => {
+        const neighbors = []
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                if (x == 0 || x == grid.length - 1 || y == 0 || y == grid[0].length - 1) neighbors.push({ num: -1, mute: 0 })
+                else {
+                    const val = grid[x + i][y + j]
+                    if (verb) console.log("x,", x, "y", y, i, j, val)
+                    neighbors.push(val)
+                }
             }
         }
+        return neighbors
     }
-    return neighbors
-}
 
-const updateCell = (grid, x, y) => {
-    if (verb) console.log("starting update of", x, y)
-    //how tho lol
-    const neighbors = getNeighbors(grid, x, y)
-    let counts = [0, 0, 0]
-    if (verb) console.log("the neighbors of", x, y, "are", neighbors)
-    neighbors.forEach(neighbor => {
-        if (verb) console.log("this neighbor is", neighbor, )
-        if (neighbor.num != -1 && neighbor.num != 0) counts[neighbor.num]++
-    })
-    const prize = { num: 1 + counts.indexOf(Math.max(...counts)), mute: max(grid[x][y].mute--, 0) }
-    grid[x][y] = prize
-}
-
-const updateGrid = () => {
-    // console.log("hi")
-    grid.forEach((column, x) => {
-        column.forEach((cell, y) => {
-            if (grid[x][y].mute > 0) updateCell(grid, x, y)
+    const updateCell = (grid, x, y) => {
+        if (verb) console.log("starting update of", x, y)
+        //how tho lol
+        const neighbors = getNeighbors(grid, x, y)
+        let counts = [0, 0, 0]
+        if (verb) console.log("the neighbors of", x, y, "are", neighbors)
+        neighbors.forEach(neighbor => {
+            if (verb) console.log("this neighbor is", neighbor, )
+            if (neighbor.num != -1 && neighbor.num != 0) counts[neighbor.num]++
         })
-    })
+        const prize = { num: 1 + counts.indexOf(Math.max(...counts)), mute: max(grid[x][y].mute--, 0) }
+        grid[x][y] = prize
+    }
+
+    const updateGrid = () => {
+        // console.log("hi")
+        grid.forEach((column, x) => {
+            column.forEach((cell, y) => {
+                if (grid[x][y].mute > 0) updateCell(grid, x, y)
+            })
+        })
+    }
 }
 
 function draw() {
-    island()
     // background(0)
     // noiseSeed(frameCount / 1000)
     // noiseMapStuff({ x: 0.02 })
