655afdf1b29513b8f4000bf41e66e9e319e9cc52
Reginald Robinson <ddrbnsn@gmail.com>
2023-06-07 18:12:53 -0700

    added constructor details

--- public/islandTypes/NoiseIsland.js
+++ public/islandTypes/NoiseIsland.js
@@ -20,59 +20,59 @@
 
 
 class NoiseIsland {
-    constructor({ cols = [
-        { name: "WaterBlue", color: { red: 20, green: 60, blue: 200 }, factor: 0 },
-        { name: "BeachTan", color: { red: 120, green: 180, blue: 30 }, factor: 1 },
-        { name: "ForestGreen", color: { red: 0, green: 255, blue: 12 }, factor: 2 },
-        { name: "RockGrey", color: { red: 100, green: 100, blue: 100 }, factor: 3 },
-        { name: "SnowWhite", color: { red: 255, green: 255, blue: 255 }, factor: 4 },
-        { name: "BrightPurple", color: { red: 200, green: 60, blue: 200 }, factor: 5 },
-    ] } = {}) {
-        this.cols = cols
+    constructor ({
+        inc = 0.1,
+        fallOff = 3,
+        center = {
+            x: (width / 2),
+            y: (height / 2)
+        },
+        seed = random(100),
+        _squareness = 0,
+        cols = [
+            { name: "WaterBlue", color: { red: 20, green: 60, blue: 200 }, factor: 0 },
+            { name: "BeachTan", color: { red: 120, green: 180, blue: 30 }, factor: 1 },
+            { name: "ForestGreen", color: { red: 0, green: 255, blue: 12 }, factor: 2 },
+            { name: "RockGrey", color: { red: 100, green: 100, blue: 100 }, factor: 3 },
+            { name: "SnowWhite", color: { red: 255, green: 255, blue: 255 }, factor: 4 },
+            { name: "BrightPurple", color: { red: 200, green: 60, blue: 200 }, factor: 5 },
+        ]
+    } = {}) {
+        this.incrementAmount = inc
+        this.fallOff = fallOff
+        this.centerPoint = center
+        this.rSeed = seed
+        this.squareness = _squareness
+        this.colors = cols
     }
 
     #pickColorByHeight = (h, thresh, x) => {
-        console.log(x)
-        for (i in this.cols) {
-            if (this.cols[i].factor == floor(h / thresh)) return this.cols[i].color
+        for (i in this.colors) {
+            if (this.colors[i].factor == floor(h / thresh)) return this.cols[i].color
         }
     }
 
-    
-    island2 = (options) => {
-        const {
-            inc = 0.1,
-            fallOff = 3,
-            center = {
-                x: (width / 2),
-                y: (height / 2)
-            },
-            seed = random(100),
-            _squareness = 0
-        } = options
-
-        console.log("updating pixels")
+    showMap = () => {
         const maxDist = dist(0, 0, center.x, center.y)
-        let yoff = 0;
+        let yoff = 0
         noiseSeed(seed)
-        loadPixels();
+        loadPixels()
         for (let y = 0; y < height; y++) {
-            let xoff = 0;
+            let xoff = 0
             for (let x = 0; x < width; x++) {
                 const distToC = dist(x, y, center.x, center.y)
                 const maskVal = map(distToC, 0, maxDist, 1, 0) ** fallOff
                 const index = (x + y * width) * 4
                 const r = noise(xoff, yoff) * maskVal
-                pixels[index + 0] = r * 255;
-                pixels[index + 1] = r * 255;
-                pixels[index + 2] = r * 255;
-                pixels[index + 3] = 255;
-                xoff += inc;
+                pixels[index + 0] = r * 255
+                pixels[index + 1] = r * 255
+                pixels[index + 2] = r * 255
+                pixels[index + 3] = 255
+                xoff += inc
             }
-            yoff += inc;
+            yoff += inc
         }
-        updatePixels();
-        console.log("updated pixels")
+        updatePixels()
     }
 
 }
